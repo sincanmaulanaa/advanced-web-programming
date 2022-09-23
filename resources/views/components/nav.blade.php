726920f918816094487ef39fb50de33a7aea17e2
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-09-23 23:56:20 +0800

    Create nav component

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,25 @@
+<style>
+    nav {
+        display: flex;
+        justify-content: center;
+    }
+
+    nav a {
+        padding: 10px;
+        text-decoration: none;
+    }
+
+    h1 {
+        text-align: center;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+
+
+<nav>
+    <a href="<?= route("beranda") ?>">Home</a>
+    <a href="<?= route("daftar") ?>">Daftar</a>
+    <a href="<?= route("masuk") ?>">Masuk</a>
+    <a href="<?= route("tamu") ?>">Tamu</a>
+    <a href="<?= route("tentang-saya") ?>">Tentang saya</a>
+</nav>
