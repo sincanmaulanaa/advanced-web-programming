37a4ee6c41d94b4f826e0438e4316e90bfdc9b33
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-10-03 18:51:58 +0800

    Add sb-admin bootstrap template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("dashboard")
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-</head>
+@section("content")
 
-<body>
-    @include("components.nav")
-    <h1>Selamat datang <?= $nama ?>.</h1>
-    <h2 style="text-align: center;">Waktu: <?= $waktu ?></h2>
-</body>
+@include("components.nav")
+<h1>Selamat datang <?= $nama ?>.</h1>
+<h2>Waktu: <?= $waktu ?></h2>
 
-</html>
+@endsection
